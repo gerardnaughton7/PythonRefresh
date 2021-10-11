6d3a9c75ac02354c3d39c42742e36946a8dec591
unknown <gerardnaughton7@gmail.com>
2021-10-10 19:44:55 -0500

    functions intro

--- pythonRefresh.py
+++ pythonRefresh.py
@@ -9,3 +9,13 @@ if name == "Nick":
     print('You are older than 18')
 else: 
     print('You are younger than 18')
+
+
+#Function
+
+def hello(name="sean", age= 0):
+    return 'Hello {} you are {} years old'.format(name, age)
+
+sentence = hello()
+
+print(sentence)
